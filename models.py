6ffcf847b3bf88d60081ce10dac807abd94b3c15
Amel Abdelraheem <38007396+AmelxJamal@users.noreply.github.com>
2022-04-27 23:41:14 +0000

    SimCLR and fineTune

--- models.py
+++ models.py
@@ -11,3 +11,36 @@ def resnet():
         nn.Linear(2048, 10, bias = True)
         )
     return model
+
+
+
+def resnet_simCLR():
+
+    model= torchvision.models.resnet50(pretrained = True)
+    for param in model.parameters():
+        param.requires_grad = True
+
+    model.fc = nn.Sequential(
+        nn.Linear(2048, 2048),
+        nn.ReLU(),
+        nn.Linear(2048,128)
+    )
+
+    return model
+
+
+def resnet_simCLR_classification(path):
+
+    model= resnet_simCLR()
+    model.load_state_dict(torch.load(path))
+
+    model.fc = nn.Linear(2048, 10)
+
+    ct = 0
+    for child in model.children():
+        ct += 1
+        if ct < 10:
+           for param in child.parameters():
+               param.requires_grad = False
+
+    return model
