76c2a31b1e78f11417a6ad00db09a9127ae02361
mohammedelabbas <100599979+mohammedelabbas@users.noreply.github.com>
2022-04-29 11:24:58 +0000

    Update models.py

--- models.py
+++ models.py
@@ -4,9 +4,6 @@ import torch
 
 def resnet():
     model= torchvision.models.resnet50(pretrained= True)
-    for param in model.parameters():
-        param.requires_grad = False
-
     model.fc = nn.Sequential(
         nn.Linear(2048, 10, bias = True)
         )
